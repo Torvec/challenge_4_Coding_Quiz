521dfed2ca5c41548fdfb789802b7d1430f31238
Edward Von Schondorf <edvonschondorf@gmail.com>
2023-05-31 16:22:06 -0700

    Final code push for grading. Yay!

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,4 +1,5 @@
 // Storing the content of the quiz in an array of objects
+
 var questions = [
     {
         question: "What is the output of the following code? console.log(typeof([]))",
@@ -23,10 +24,10 @@ var questions = [
     {
         question: "What does the '===' operator do in JavaScript?",
         answers: [
-            "Checks for equality of values",
-            "Checks for equality of values and types",
+            "Checks for equality of the values",
+            "Checks for equality of the values and data types",
             "Assigns a value to a variable",
-            "Compare two variables lexicographically"
+            "Compares two values and returns true if they are the same type"
         ],
         correctAnswer: 1
     },
@@ -34,9 +35,9 @@ var questions = [
         question: "What is the scope of a variable with the 'var' keyword?",
         answers: [
             "Global scope",
-            "Local scope",
-            "Block scope",
-            "Function scope"
+            "Function Scope or Module Scope",
+            "Block scope or Local Scope",
+            "Function scope or Global Scope"
         ],
         correctAnswer: 3
     },
@@ -46,8 +47,58 @@ var questions = [
             "Pauses the execution of the program for a specified amount of time",
             "Executes a function after a specified amount of time has passed",
             "Sets an interval to repeatedly execute a function",
-            "Halts the program execution indefinitely"
+            "Halts the function execution indefinitely"
         ],
         correctAnswer: 1
+    },
+    {
+        question: "What is the official name of JavaScript?",
+        answers: [
+            "ActionScript",
+            "TYpeScript",
+            "CoffeeScript",
+            "ECMAScript"
+        ],
+        correctAnswer: 3
+    },
+    {
+        question: "Which of the following will increase a value by 2?",
+        answers: [
+            "x++",
+            "x *= 2",
+            "x += 2",
+            "x += 2x"
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "What type of loop will continue to iterate so long as the set condition is true?",
+        answers: [
+            "do...while",
+            "For...In",
+            "while",
+            "for"
+        ],
+        correctAnswer: 2
+    },
+    {
+        question: "Which of the following statements about the setInterval function in JavaScript is true?",
+        answers: [
+            "It executes a function repeatedly at a specified interval.",
+            "It executes a function only once after a specified delay",
+            "It pauses the execution of a function for a specified duration.",
+            "It sets a timeout for a function to be executed after a specific time."
+        ],
+        correctAnswer: 0
+    },
+    {
+        question: "What would the value of x be after this while loop completed: var x = 1; while(x <= 9000) { x += 2; }",
+        answers: [
+            "9002",
+            "9000",
+            "9001",
+            "Over 9000"
+        ],
+        correctAnswer: 3
     }
 ];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
 // Created by Edward Von Schondorf on May 2023
 
-// FIX HIGHEST SCORE NOT SHOWING UP UNDER HI SCORE
-// FIX TIMER NOT STOPPING WHEN YOU FINISH ANSWERING ALL QUESTIONS
-
 // Declaring Global Variables
 var main = document.querySelector("main");
 var score = document.getElementById("scoreValue");
@@ -10,12 +7,16 @@ var timer = document.getElementById("timerValue");
 var hiScore = document.getElementById("hiScoreValue");
 var timeLeft = 30;
 var scoreValue = 0;
+
 // Get the high score from local storage if it exists, otherwise set it to 0
 var userEntries = localStorage.getItem("userEntries");
 var parsedEntry = JSON.parse(userEntries);
 if(!parsedEntry) {
     var hiScoreValue = 0;
 } else {
+    parsedEntry.sort(function(a, b) {
+        return b.score - a.score; 
+    });
     hiScoreValue = parsedEntry[0].score;
 }
 
@@ -26,39 +27,35 @@ function clearMain() {
 
 // StartUp function runs when the page is loaded or when the game is restarted
 function startUp() {
-
-    clearMain();
-
     // Create the title
     var javascriptQuizTitle = document.createElement("h1");
         javascriptQuizTitle.textContent = "JavaScript Quiz";
         main.appendChild(javascriptQuizTitle);
-
     // Create the introduction
     var introduction = document.createElement("p");
         introduction.textContent = "Welcome to the JavaScript Multiple Choice Quiz! In this timed game, you'll test your knowledge of JavaScript. Each correct answer will earn you 1 point, while an incorrect answer will deduct 5 seconds from the remaining time. The game will end when either all questions are answered or the timer reaches 0. Good luck and have fun!";
         main.appendChild(introduction);
-
     // Create the button group and buttons
     var btnGroup = document.createElement("div");
         btnGroup.setAttribute("class", "btnGroup");
         main.appendChild(btnGroup);
-
     var startButton = document.createElement("button");
         startButton.setAttribute("id", "startButton");
         startButton.textContent = "Start Quiz";
         btnGroup.appendChild(startButton);
-
     var viewScoresButton = document.createElement("button");
         viewScoresButton.setAttribute("id", "viewScoresButton");
         viewScoresButton.textContent = "View High Scores";
         btnGroup.appendChild(viewScoresButton);
-
     startButton.addEventListener("click", function() {
+        clearMain();
         askQuestion(0);
         countdown();
     });
-    viewScoresButton.addEventListener("click", scoreboard);
+    viewScoresButton.addEventListener("click", function() {
+        clearMain();
+        scoreboard();
+    });
     // Set initial values
     score.textContent = scoreValue;
     timer.textContent = timeLeft;
@@ -67,26 +64,19 @@ function startUp() {
 
 // Ask the question and display the answers
 function askQuestion(questionNumber) {
-   
-    clearMain();
-
     var questionTitle = document.createElement("h2");
         questionTitle.setAttribute("class", "question");
         questionTitle.textContent = questions[questionNumber].question;
         main.appendChild(questionTitle);
-
     var btnGroup = document.createElement("div");
         btnGroup.setAttribute("class", "btnGroup");
         main.appendChild(btnGroup);
-
-    for (var i = 0; i < questions[questionNumber].answers.length; i++) {
-        
+    for (var i = 0; i < questions[questionNumber].answers.length; i++) {       
         var answerBtn = document.createElement("button");
             answerBtn.setAttribute("class", "answerBtn");
             answerBtn.setAttribute("id", "answer" + i);
             answerBtn.textContent = questions[questionNumber].answers[i];
             btnGroup.appendChild(answerBtn);
-
         var selectedAnswer = document.querySelector("#answer" + i);
         selectedAnswer.addEventListener("click", function() {            
             if (this.textContent === questions[questionNumber].answers[questions[questionNumber].correctAnswer]) {
@@ -125,12 +115,12 @@ function nextQuestion(questionNumber){
         setTimeout(function() {
             clearMain();
             askQuestion(questionNumber);
-        }, 1000);
+        }, 500);
     } else {
         setTimeout(function() {
             clearMain();
             endQuiz();
-        }, 1000);
+        }, 500);
     }
 }
 
@@ -143,6 +133,7 @@ function countdown() {
         } else {
             timer.textContent = 0;
             clearInterval(timeInterval);
+            clearMain()
             endQuiz();
         }
     }, 1000);
@@ -160,19 +151,19 @@ function subtractTime() {
     timer.textContent = timeLeft;
 }
 
-// EndQuiz function runs when the timer reaches 0 or all questions are answered
 function endQuiz() {
-    
-    clearMain()
-    
+    // Stops the timer if user answers all questions
+    timeLeft = 0;
+    timer.textContent = timeLeft;
+    // Create the quiz over title
     var quizOverTitle = document.createElement("h1");
         quizOverTitle.textContent = "Quiz Over!";
         main.appendChild(quizOverTitle);
-
+    // Create the form
     var submitScoreForm = document.createElement("form");
         submitScoreForm.setAttribute("id", "submitScoreForm");
         main.appendChild(submitScoreForm);
-
+    // Create the initials input field
     var initialsInput = document.createElement("input");
         initialsInput.setAttribute("id", "initialsInput");
         initialsInput.setAttribute("type", "text");
@@ -180,13 +171,13 @@ function endQuiz() {
         initialsInput.setAttribute("required", "true");
         initialsInput.setAttribute("placeholder", "Enter Your Initials");
         submitScoreForm.appendChild(initialsInput);
-
+    // Create the submit score button
     var submitScoreBtn = document.createElement("button");
         submitScoreBtn.setAttribute("id", "submitScoreBtn");
         submitScoreBtn.textContent = "Submit Score"; 
         submitScoreForm.appendChild(submitScoreBtn);  
-
     submitScoreBtn.addEventListener("click", function() {
+        // Prevent form from refreshing the page
         event.preventDefault();
         //Get the value of the input field
         var initialsInput = document.getElementById("initialsInput").value; 
@@ -194,13 +185,14 @@ function endQuiz() {
         var initials = initialsInput.toUpperCase(); 
         // Create a string of the alphabet to check against
         var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
-        // Check if the initials are letters and display an alert if they are not
+        // Prevent user from entering anything other than letters
         for (var i = 0; i < initials.length; i++) {
             if (alphabet.indexOf(initials[i]) === -1) {
                 alert("Please enter only letters A-Z");
                 return;
             }
         }
+        // Prevent user from entering a blank value or a single letter
         if (initials.length < 2) {
             alert("Please enter 2 letters");
             return;
@@ -220,13 +212,16 @@ function endQuiz() {
         // Push the object to the array
         entries.push(userEntry);
         localStorage.setItem("userEntries", JSON.stringify(entries));
+        clearMain()
         scoreboard();
     });    
 }
 
-// Submit Score function runs when the submit score button is clicked            
 function scoreboard() {
-    clearMain()
+    // Create the high scores title
+    var highScoresTitle = document.createElement("h1");
+        highScoresTitle.textContent = "High Scores";
+        main.appendChild(highScoresTitle);
     // Get the high score from local storage if it exists, otherwise set it to 0
     var userEntries = localStorage.getItem("userEntries");
     var parsedEntry = JSON.parse(userEntries); 
@@ -235,46 +230,48 @@ function scoreboard() {
     } else {
         // Sort in descending order by score
         parsedEntry.sort(function(a, b) {
-            return b.score - a.score; 
+            return b.score - a.score;
         })
     }
-    // Create the high scores title
-    var highScoresTitle = document.createElement("h1");
-        highScoresTitle.textContent = "High Scores";
-        main.appendChild(highScoresTitle);
     // Create the score list and populate it with the scores from local storage if there are any
     if (parsedEntry.length === 0) {
+        // Create a message if no scores exist
         var noScoreItem = document.createElement("p");
             noScoreItem.textContent = "No scores yet!";
             noScoreItem.style.textAlign = "center";
             main.appendChild(noScoreItem);
     } else {
+        // Update the high score if a new high score is achieved
+        hiScoreValue = parsedEntry[0].score;
+        hiScore.textContent = hiScoreValue;
+        // Create the score list if scores exist
         var scoreList = document.createElement("ol");
         scoreList.setAttribute("id", "scoreList");
         main.appendChild(scoreList);
-    }
-        for( var i = 0; i < parsedEntry.length; i++ ) {
-
+        // Create the score list items and populate them with the initials and score
+        for(var i = 0; i < parsedEntry.length; i++) {
+            // Create a list item to hold the initials and score and add it to the score list
             var scoreItem = document.createElement("li");
                 scoreList.appendChild(scoreItem);
-
+            // Create a span to hold the initials and add it to the score item
             var scoreList_Initials = document.createElement("span");
                 scoreList_Initials.setAttribute("class", "scoreList_Initials");
                 scoreList_Initials.textContent = parsedEntry[i].initials;
                 scoreItem.appendChild(scoreList_Initials);
-
+            // Create a span to hold the score and add it to the score item
             var scoreList_Score = document.createElement("span");
                 scoreList_Score.setAttribute("class", "scoreList_Score");
                 scoreList_Score.textContent = parsedEntry[i].score;
                 scoreItem.appendChild(scoreList_Score);
+        }
     }
     // Create replay button and restart the game when clicked
     var replayBtn = document.createElement("button");
         replayBtn.setAttribute("id", "replayBtn");
         replayBtn.textContent = "Replay";
         main.appendChild(replayBtn);
-
     replayBtn.addEventListener("click", function(){
+        clearMain();
         startUp();
         timeLeft = 30;
         timer.textContent = timeLeft;
@@ -286,7 +283,6 @@ function scoreboard() {
         clearScoresBtn.setAttribute("id", "clearScoresBtn");
         clearScoresBtn.textContent = "Clear Scores";
         main.appendChild(clearScoresBtn);
-
     clearScoresBtn.addEventListener("click", function(){
         if (parsedEntry.length === 0) {
             alert("There are no scores to clear!");
@@ -295,6 +291,8 @@ function scoreboard() {
             confirm("Are you sure you want to clear the scoreboard?");
             localStorage.clear();
             scoreList.innerHTML = "";
+            hiScoreValue = 0;
+            hiScore.textContent = hiScoreValue;
         }
     });
     // Create a button group to hold the buttons
@@ -305,4 +303,6 @@ function scoreboard() {
         btnGroup.appendChild(clearScoresBtn);
 }
 
-startUp();
+startUp();// Call the startUp function to begin the quiz. 
+
+// Kind of ironic that the start of the program is at the end. Honestly, I can't believe I got this to work. My brain hurts. 
